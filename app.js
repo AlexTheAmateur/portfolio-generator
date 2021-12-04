b523d4bb9a7cfefdc71fb32d747923bf652faba6
AlexTheAmateur <awu4921@gmail.com>
2021-12-04 11:33:11 -0600

    Page generation js

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-// const printProfileData = profileDataArr => {
-//     for (let i = 0; i < profileDataArr.length; i++) {
-//       console.log(profileDataArr[i]);
-//     }
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-//   console.log('==============================');
+const profileDataArgs = process.argv.slice(2);
 
-// //profileDataArr.forEach(profileItem => console.log(profileItem));
-//   profileDataArr.forEach((profileItem) => {
-//       console.log(profileItem)
-//   });
-// };
+const [name, github] = profileDataArgs;
 
-// printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage('Alex', 'AlexTheAmateur'), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
